f9c6f5addefd5ae374a22a32f2089eb2dfc5f935
Bolaji Olajide <bolaji.olajide@andela.com>
2018-09-08 02:48:05 +0100

    add comment to export functions

--- user.go
+++ user.go
@@ -5,18 +5,22 @@ import (
 	"net/http"
 )
 
+// AllUsers - method for getting all users
 func AllUsers(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "All Users Endpoint Hit")
 }
 
+// NewUser - method for creating a new user
 func NewUser(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "New user endpoint hit")
 }
 
+// DeleteUser - method for deleting a user
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Delete endpoint hit")
 }
 
+// UpdateUser - method for updating a new user
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Update endpoint hit")
 }
